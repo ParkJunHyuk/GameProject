cd8275ac58db5dc4820d514b6526aa0fb3092703
jungmu <whdrjs0@gmail.com>
2016-11-17 21:36:02 +0900

    Revert "[정종선] 디렉토리 구조 변경"
    This reverts commit fec8104fc3885f55ae6bce77afa0f75e8be06a49.
    
    메인 로딩 씬 추가

---
+++ Master/Assets/scripts/loading.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class loading : MonoBehaviour {
+
+    public Text loadingText;
+    
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Progress());
+        StartCoroutine(ChangeScene());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Progress()
+    {
+        while(true)
+        {
+            loadingText.GetComponent<Text>().text = loadingText.GetComponent<Text>().text+".";
+            yield return new WaitForSeconds(1);
+        }
+       
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(1);
+    }
+}
